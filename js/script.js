3f079c9983e7503748e2f5586801b6e7d39fef51
Carlos Destefanis <carlosidestefanis@gmail.com>
2023-07-27 21:56:14 -0300

    formulario de contacto con envio de email con JS

--- js/script.js
+++ js/script.js
@@ -31,3 +31,17 @@ var TrandingSlider = new Swiper('.tranding-slider', {
     }
   });
   
+  // EMVIO DE EMAIL FORMULARIO
+
+  
+  const $form = document.querySelector('#form')
+  const $buttonMailto = document.querySelector('#trucazo')
+
+  $form.addEventListener('submit', handleSubmit)
+
+  function handleSubmit(event) {
+    event.preventDefault()
+    const form = new FormData(this)
+    $buttonMailto.setAttribute('href', `mailto:carlosidestefanis@gmail.com?subject=${form.get('name')}${form.get('email')}&body=${form.get('message')}`)
+    $buttonMailto.click()
+  }
